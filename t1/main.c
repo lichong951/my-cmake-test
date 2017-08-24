3bf2bbd99529c67fe31353a47ae65b15c954b7fd
lichong <lichongmac@163.com>
2017-08-24 14:58:22 +0800

    init && add t1

---
+++ t1/main.c
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  MyC
+//
+//  Created by lichong on 2017/8/16.
+//  Copyright © 2017年 lichong. All rights reserved.
+//
+#include <stdio.h>
+int main()
+{
+printf(“Hello World from t1 Main!\n”);
+return 0;
+}
